63924ffd54fa37943a68729c41cc3298e423bd5b
Manjitha-MK <manjithataridu123@gmail.com>
2025-06-08 10:15:01 +0530

    Add tuple for user info and enum for user roles😎👍

--- dist/index.js
+++ dist/index.js
@@ -11,5 +11,16 @@ let user = {
 function multiply(a, b) {
     return a * b;
 }
+let userInfo = ["Kamal", 20];
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["User"] = 1] = "User";
+    Role[Role["Guest"] = 2] = "Guest";
+    Role[Role["Customer"] = 3] = "Customer";
+})(Role || (Role = {}));
+let myRole = Role.Customer;
 console.log(Name, age, isStudent, hobbies, marks, user);
-console.log("multiply: ", multiply(3, 4));
+console.log("multiply:", multiply(3, 4));
+console.log("user:", userInfo[0], "Age:", userInfo[1]);
+console.log("My role:", myRole);
--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,18 @@ function multiply(a: number, b: number): number{
     return a * b ;
 }
 
+let userInfo: [string, number] = ["Kamal", 20];
+
+enum Role {
+    Admin,
+    User,
+    Guest,
+    Customer
+}
+let myRole: Role =  Role.Customer;
+
 console.log(Name, age, isStudent, hobbies, marks, user);
-console.log("multiply: ",multiply(3,4));
+console.log("multiply:",multiply(3,4));
+console.log("user:",userInfo[0], "Age:",userInfo[1]);
+console.log("My role:",myRole);
+
